1dc0545938439d0a421b866bbabf63430036203a
Yan Lin <lyan@cs.aau.dk>
2024-10-25 11:36:57 +0200

    Remove a few files

--- upload.sh
+++
@@ -1,4 +0,0 @@
-rsync -avP --delete ./src/ aicloud:~/pub-summarizer/src --exclude "*.pyc"
-rsync -avP ./requirements.txt aicloud:~/pub-summarizer/
-rsync -avP ./config.yaml aicloud:~/pub-summarizer/
-rsync -avP ./pubsum.def aicloud:~/pub-summarizer/
